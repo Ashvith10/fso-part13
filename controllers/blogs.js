8b2ea7f63ad713ee96e9372dda26060eb5f8bc65
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-12-02 23:02:45 +0530

    feat: Implement support to update the like of a blog
    Add a PUT request to update the likes of the blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,6 +3,11 @@ import { Blog } from '../models/index.js';
 
 const router = Router();
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+}
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
@@ -17,13 +22,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    await blog.destroy();
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
     res.status(204).end();
   } else {
-    res.status(410).end();
+    res.status(404).end();
+  }
+});
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes;
+    await req.blog.save()
+    res.json({ likes: req.blog.likes });
+  } else {
+    res.status(404).end();
   }
 });
 
